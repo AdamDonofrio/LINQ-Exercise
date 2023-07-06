9eb29ea5a968967522825a645c576aafc205b837
unknown <donofrio.8@wright.edu>
2023-07-06 11:26:19 -0400

    first commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace LINQ
+﻿using System;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
+
+        private static string[] videoGamesList = { "Psychonauts", "Halo", "Team Fortris 2", "Starfield", "Psychonauts 2"};
+        //private static string[] newGameList = { "" };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*var newGameList = from game in videoGamesList
+                              where game != null
+                              orderby game ascending
+                              select game;
+            */
+
+            var newGameList = videoGamesList.OrderBy(x => x.Length).ToList();
+            
+
+            foreach (var i in videoGamesList)
+            {
+                Console.WriteLine(i.ToString());
+            }
+            
+            
+            Console.WriteLine();
+
+            foreach (var i in newGameList)
+            {
+                Console.WriteLine(i.ToString());
+            }
         }
     }
 }
